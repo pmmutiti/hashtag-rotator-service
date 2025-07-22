36d8c964b874c372dd25e53baa79a6e67f593fb0
Peter M. Mutiti <52533415+pmmutiti@users.noreply.github.com>
2025-07-22 11:11:56 +0300

    Update cron.js
    Signed-off-by: Peter M. Mutiti <52533415+pmmutiti@users.noreply.github.com>

--- cron.js
+++ cron.js
@@ -1,5 +1,4 @@
 
 { "crons": [ { "path": "/api/cron", "schedule": "0 10 * * *" } ] } 
 
-
 export default async function handler(req, res) { const now = new Date().toISOString(); // 🔁 Log to Vercel's serverless function log output console.log("Cron triggered at", now); res.status(200).json({ message: "Cron ran successfully", timestamp: now }); } 
