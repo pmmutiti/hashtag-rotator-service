b0534e6270b04a10bbc3883fadd1eab9a01ba83b
Peter M. Mutiti <52533415+pmmutiti@users.noreply.github.com>
2025-08-05 23:44:38 +0300

    Update hashtag-rotator-service.js
    Signed-off-by: Peter M. Mutiti <52533415+pmmutiti@users.noreply.github.com>

--- hashtag-rotator-service.js
+++ hashtag-rotator-service.js
@@ -1,1 +1,2 @@
 
+export default async function handler(req, res) { const event = req.headers['x-github-event']; console.log(`[Webhook: hashtag-rotate-service] Event: ${event}`); res.status(200).json({ received: true, event }); } 
