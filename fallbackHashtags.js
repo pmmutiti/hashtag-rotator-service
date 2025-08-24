31b106dccd7945fbbed1e44e93b6ab238e627a5b
Peter M. Mutiti <52533415+pmmutiti@users.noreply.github.com>
2025-08-24 22:17:14 +0300

    Update and rename fallback.js to fallbackHashtags.js
    Signed-off-by: Peter M. Mutiti <52533415+pmmutiti@users.noreply.github.com>

---
+++ fallbackHashtags.js
@@ -0,0 +1,18 @@
+export const fallbackHashtags = {
+  kenya: [
+    "#JusticeforJuliaNjoki",
+    "#ShootFirstSir",
+    "#WeAreAllKikuyus"
+  ],
+  usa: [
+    "#BaddiesMidwest",
+    "#Election2025"
+  ],
+  uk: [
+    "#BritishGP"
+  ],
+  global: [
+    "#BuildOverride",
+    "#DashboardSilence"
+  ]
+};
